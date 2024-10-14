cf84caf9ab72e19164e5f005a1ab423d22717587
Leo-silva1707 <rodriguez.silva.leonardo@iescamas.es>
2024-10-14 09:56:14 +0200

    Add files via upload

---
+++ ejercicio7.py
@@ -0,0 +1,9 @@
+#Este programa hace la nota media entre 3 notas
+print("¿cual es tu primera nota?")
+nota1 = float(input())
+print("¿cual es tu segunda nota?")
+nota2 = float(input())
+print("¿cual es tu tercera nota?")
+nota3 = float(input())
+media = (nota1+nota2+nota3)/3
+print("El resultado es" ,media)
---
+++ ejercicio8.py
@@ -0,0 +1,6 @@
+print("¿en que año estamos?")
+año1 = float(input())
+print("¿en que año naciste?")
+año2 = float(input())
+años = año1 - año2
+print("Tu edada es:" ,años)
